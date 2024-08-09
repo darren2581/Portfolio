918151dc6e42917ca7a9df837d508bc689dd9d96
darren2581 <darrentanthongen@gmail.com>
2024-08-09 19:36:56 +0800

    Testing vs to github

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Portfolio</title>
+        <title>Github</title>
       </head>
       <body id="home">
         <div class="header">
